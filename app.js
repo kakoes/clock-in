6706d4645cbab5c3382f9fb90d4b7be3df92b105
Muhammad Azeem <csgonoobism@gmail.com>
2026-05-18 22:34:26 -0500

    RDC UI foundation

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-console.log('Clock-In Loaded');
+console.log('RDC Terminal Loaded');
+
+const clockInTab =
+  document.getElementById('clockInTab');
+
+const clockOutTab =
+  document.getElementById('clockOutTab');
+
+const clockInForm =
+  document.getElementById('clockInForm');
+
+const clockOutForm =
+  document.getElementById('clockOutForm');
+
+clockInTab.addEventListener(
+  'click',
+  () => {
+
+    clockInTab.classList.add('active');
+
+    clockOutTab.classList.remove('active');
+
+    clockInForm.classList.remove('hidden');
+
+    clockOutForm.classList.add('hidden');
+  }
+);
+
+clockOutTab.addEventListener(
+  'click',
+  () => {
+
+    clockOutTab.classList.add('active');
+
+    clockInTab.classList.remove('active');
+
+    clockOutForm.classList.remove('hidden');
+
+    clockInForm.classList.add('hidden');
+  }
+);
